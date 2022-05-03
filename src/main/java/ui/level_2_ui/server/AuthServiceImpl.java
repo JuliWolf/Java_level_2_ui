d603b6e415863884dd7235376754c2ce4a0ffee2
Julia Ivanova <for.julia96@gmail.com>
2022-05-03 13:06:55 +0300

    fix

--- src/main/java/ui/level_2_ui/server/AuthServiceImpl.java
+++ src/main/java/ui/level_2_ui/server/AuthServiceImpl.java
@@ -1,32 +1,10 @@
 package ui.level_2_ui.server;
 
-import java.util.ArrayList;
-import java.util.List;
-
 public class AuthServiceImpl implements AuthService {
     private final DataBaseConnect dbConnection;
 
-    private class Entry {
-        private final String login;
-        private final String pass;
-        private final String nick;
-
-
-        private Entry(String login, String pass, String nick) {
-            this.login = login;
-            this.pass = pass;
-            this.nick = nick;
-        }
-    }
-
-    private List<Entry> entries;
-
     public AuthServiceImpl(DataBaseConnect connect) {
         this.dbConnection = connect;
-        entries = new ArrayList<>();
-        for (int i = 0; i < 5; i++) {
-            entries.add(new Entry("login"+i, "pass"+i, "nick"+i));
-        }
     }
 
     @Override
